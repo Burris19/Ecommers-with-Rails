140a6c878802f515eeaad13c24b188affcaa0ec1
Julian Hernandez <julianhernandez19@gmail.com>
2016-02-25 02:10:13 -0600

    [add] unit tests forn products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it{ should validate_presence_of :name }
+  it{ should validate_presence_of :user }
+  it{ should validate_presence_of :pricing }
+
+  it "should validte pricing > 0" do
+  	#con create lo guarda en la base de datos
+  	#con build solamente lo guarda en memoria
+  	#product = FactoryGirl.create(:product)
+  	product = FactoryGirl.build(:product, pricing: 0)
+  	#imprimo el producto
+  	#puts product.inspect
+
+  	#le indico que me muestre los errores 
+  	puts product.errors.inspect
+
+  	#espero que sea falso para que la prueba pase
+  	expect(product.valid?).to be_falsy
+
+
+  end
+
 end
