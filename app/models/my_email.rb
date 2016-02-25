af0756ce4ee112d9d5bee5765cc103d3064bbc25
Julian Hernandez <julianhernandez19@gmail.com>
2016-02-25 01:03:01 -0600

    [add] validate in format email

--- app/models/my_email.rb
+++ app/models/my_email.rb
@@ -1,5 +1,5 @@
 class MyEmail < ActiveRecord::Base
 	validates_presence_of :email, message: "El email no puede ser nulo"
 	validates_uniqueness_of :email, message: "El email ya esta registrado"
-	validates_format_of :email, with: Devise::email_regexp
+	validates_format_of :email, with: Devise::email_regexp, message: "Correo no valido"
 end
