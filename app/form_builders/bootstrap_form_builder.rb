67644859c6fadd8118c3a5efbfc3d8b7c5395b24
Julian Hernandez <julianhernandez19@gmail.com>
2016-02-26 00:02:16 -0600

    [update] refactoring form builder for create method dinamic

--- app/form_builders/bootstrap_form_builder.rb
+++ app/form_builders/bootstrap_form_builder.rb
@@ -2,15 +2,52 @@ class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
 
 	delegate :content_tag, to: :@template
 
-	def text_field name, *args
-		options = args.extract_options!
-		if options.has_key?(:class)
-			options[:class] += "form-control"
-		else
-			options[:class] = "form-control"
+	# def text_field name, *args
+	# 	options = args.extract_options!
+	# 	if options.has_key?(:class)
+	# 		options[:class] += "form-control"
+	# 	else
+	# 		options[:class] = "form-control"
+	# 	end
+	# 	content_tag :div, class:"form-group" do
+	# 		content_tag(:label, name , name:name) + super(name, options)
+	# 	end
+	# end	
+
+	# def text_area name, *args
+	# 	options = args.extract_options!
+	# 	if options.has_key?(:class)
+	# 		options[:class] += "form-control"
+	# 	else
+	# 		options[:class] = "form-control"
+	# 	end
+	# 	content_tag :div, class:"form-group" do
+	# 		content_tag(:label, name , name:name) + super(name, options)
+	# 	end
+	# end	
+	#aca refactorizamos los dos metos anteriores ya que son casi lo mismo 
+
+	
+	[:text_field, :text_area, :url_field, :email_field, :number_field].each do |metodo|
+		define_method metodo do |name, *args|
+		 	options = args.extract_options!
+			if options.has_key?(:class)
+				options[:class] += "form-control"
+			else
+				options[:class] = "form-control"
+			end
+			content_tag :div, class:"form-group" do
+				content_tag(:label, name , name:name) + super(name, options)
+			end
 		end
-		content_tag :div, class:"form-group" do
-			content_tag(:label, name , name:name) + super(name, options)
-		end
-	end	
+	end
+
+
+	def submit(*args) 
+		content_tag :div do
+			super(*args, class: "btn btn-success")
+		end	
+	end
+
+
 end
